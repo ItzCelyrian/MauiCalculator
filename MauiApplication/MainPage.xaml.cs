168499df91a93478ac4d676f5306b09b088521a0
ItzCelyrian <itzcelyrian@gmail.com>
2022-11-16 13:40:56 +0100

    Added ANS function, optimized code, fixed exception handling

--- MauiApplication/MainPage.xaml.cs
+++ MauiApplication/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Linq;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -8,6 +9,7 @@ namespace MauiApplication;
 public partial class MainPage : ContentPage
 {
     string entry;
+    string ans;
     public MainPage()
     {
         InitializeComponent();
@@ -24,7 +26,7 @@ public partial class MainPage : ContentPage
             return (double)(DataTable.Rows[0]["Eval"]);
         }
         catch
-        {
+        { 
             return 0;
         }
     }
@@ -33,59 +35,59 @@ public partial class MainPage : ContentPage
         string[] zeroOps = { "+0", "-0", "*0", "/0" };
         bool zeroCase = false;
 
-        if ( DisplayEntry.Text is not null )
-            zeroCase = zeroOps.Any(x => DisplayEntry.Text.EndsWith(x)) || DisplayEntry.Text == "0";
+        if ( Display.Text is not null )
+            zeroCase = zeroOps.Any(x => Display.Text.EndsWith(x)) || Display.Text is "0";
 
-        if ( (sender as Button).Text == "0" && zeroCase )
+        if ( (sender as Button).Text is "0" && zeroCase )
             return;
 
-        if ( DisplayEntry.Text is not null && (sender as Button).Text != "0" && DisplayEntry.Text.EndsWith("0") ) 
-            DisplayEntry.Text = DisplayEntry.Text.Substring(0, DisplayEntry.Text.Length - 1);
+        if ( zeroCase && (sender as Button).Text is not "0" && Display.Text.EndsWith("0") ) 
+            Display.Text = Display.Text.Substring(0, Display.Text.Length - 1);
         
         entry += (sender as Button).Text;
-        DisplayEntry.Text = entry;
+        Display.Text = entry;
     }
-    private void ClickC(object sender, EventArgs e)
+    private void Clear(object sender, EventArgs e)
     {
         entry = null;
-        DisplayEntry.Text = entry;
-        DisplayAlert("Cleared", "Cleared the Calculator.", "OK");
+        Display.Text = entry;
+        DisplayAlert("Success", "The Calculator has been cleared.", "OK");
     }
-    private void ClickE(object sender, EventArgs e)
+    private async void Equals(object sender, EventArgs e)
     {
-        entry = Convert.ToString(Evaluate(DisplayEntry.Text)).Replace(",", ".");
-        if (entry == null || entry == "NaN")
+        entry = Convert.ToString(Evaluate(Display.Text.Replace("Ans", ans))).Replace(",", ".");
+        
+        if (entry is "NaN") 
             entry = "Undefined";
-        DisplayEntry.Text = entry;
+
+        bool answer = await DisplayAlert($"Answer: {entry}", "Do you want to save your Answer as the ANS variable?", "Yes", "No");
+        if (answer)
+            ans = entry;
     }
-    private void Add(object sender, EventArgs e)
+    private void Op(object sender, EventArgs e)
     {
-        if (entry == null || entry.EndsWith("+") || entry.EndsWith("-") || entry.EndsWith("*") || entry.EndsWith("/")) return;
-        else entry = entry + "+";
-        DisplayEntry.Text = entry;
+        string[] operators = { "+", "-", "*", "/" };
+        bool endOperator = true;
+
+        if (Display.Text is not null)
+            endOperator = operators.Any(x => Display.Text.EndsWith(x));
+                
+        if ( endOperator )
+            Display.Text = Display.Text.Substring(0, Display.Text.Length - 1);
+
+        entry += (sender as Button).Text;
+        Display.Text = entry;
     }
-    private void Sub(object sender, EventArgs e)
+    private void Update(object sender, TextChangedEventArgs e)
     {
-        if (entry == null || entry.EndsWith("+") || entry.EndsWith("-") || entry.EndsWith("*") || entry.EndsWith("/")) return;
-        else entry = entry + "-";
-        DisplayEntry.Text = entry;
-    }
-    private void Mult(object sender, EventArgs e)
-    {
-        if (entry == null || entry.EndsWith("+") || entry.EndsWith("-") || entry.EndsWith("*") || entry.EndsWith("/")) return;
-        else entry = entry + "*";
-        DisplayEntry.Text = entry;
-    }
-    private void Div(object sender, EventArgs e)
-    {
-        if (entry == null || entry.EndsWith("+") || entry.EndsWith("-") || entry.EndsWith("*") || entry.EndsWith("/")) return;
-        else entry = entry + "/";
-        DisplayEntry.Text = entry;
+        entry = Display.Text;
     }
 
-    private void UpdateEntry(object sender, TextChangedEventArgs e)
+    private void Backspace(object sender, EventArgs e)
     {
-        entry = DisplayEntry.Text;
+        if (Display.Text.Length > 0)
+            Display.Text = Display.Text.Substring(0, Display.Text.Length - 1);
+        else
+            DisplayAlert("Error", "The Entry is already empty.", "OK");
     }
 }
-
